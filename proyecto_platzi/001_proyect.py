9d7a720d51c27d0fa760de8511e42969d4827396
Gabinete <fcisneros@outlook.es>
2024-12-14 16:48:25 -0600

    proyecto platzi

---
+++ proyecto_platzi/001_proyect.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import os
+
+os.system('clear')
+
+# Cargar el dataset
+file_path = '~/numpy_pandas_matplotlib/proyecto_platzi/Online_Retail.csv'
+data = pd.read_csv(file_path, encoding='windows-1252')
+pd.set_option('display.max_columns', None)
+#  print(data.info())
+#  print(data.head())
+#  #  print(data.describe())
+#  print(data.isnull().sum())
+#  print(data.duplicated().sum())
+
+unique_values = {col: data[col].unique() for col in data.columns}
+for col, values in unique_values.items():
+    print(f'Columna: {col}')
+    print(f'Numero de valores: {len(values)}')
+    print(f'Valores únicos: {values[:10]}')
+    print('-'*100)
