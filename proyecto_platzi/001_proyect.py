47b432594930d022414a0cf2b0a595e7a50cee38
Francisco Cisneros <fcisnerosr@outlook.es>
2024-12-14 18:32:21 -0600

    actualizacion

--- proyecto_platzi/001_proyect.py
+++ proyecto_platzi/001_proyect.py
@@ -4,7 +4,8 @@ import os
 os.system('clear')
 
 # Cargar el dataset
-file_path = '~/numpy_pandas_matplotlib/proyecto_platzi/Online_Retail.csv'
+#  file_path = '~/numpy_pandas_matplotlib/proyecto_platzi/Online_Retail.csv'
+file_path = '~/Documents/numpy_pandas_matplotlib/proyecto_platzi/Online_Retail.csv'
 data = pd.read_csv(file_path, encoding='windows-1252')
 pd.set_option('display.max_columns', None)
 #  print(data.info())
@@ -18,4 +19,4 @@ for col, values in unique_values.items():
     print(f'Columna: {col}')
     print(f'Numero de valores: {len(values)}')
     print(f'Valores únicos: {values[:10]}')
-    print('-'*100)
+    print('-'*50)
