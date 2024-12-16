3fc6abdfdade6bea5055cfde130aad9f7410579a
Gabinete <fcisneros@outlook.es>
2024-12-16 10:18:20 -0600

    actualizacion

--- proyecto_platzi/001_proyect.py
+++ proyecto_platzi/001_proyect.py
@@ -19,4 +19,27 @@ for col, values in unique_values.items():
     print(f'Columna: {col}')
     print(f'Numero de valores: {len(values)}')
     print(f'Valores únicos: {values[:10]}')
+<<<<<<< HEAD
     print('-'*50)
+=======
+    print('-'*100)
+
+# Limpieza de datos
+data_cleaned = data.drop_duplicates()
+data_cleaned = data_cleaned.dropna(subset=['CustomerID'])
+print(data_cleaned)
+print(data_cleaned.isnull().sum())
+print(data_cleaned.duplicated().sum())
+
+# Creacion de columnas
+print(data_cleaned.head())
+data_cleaned['TotalAmount'] = data_cleaned['Quantity'] * data_cleaned['UnitPrice']
+print(data_cleaned.head())
+data_cleaned['InvoiceDate'] = pd.to_datetime(data_cleaned['InvoiceDate'])
+print(data_cleaned.head())
+print(data_cleaned.info())
+data_cleaned['Year'] = data_cleaned['InvoiceDate'].dt.year
+data_cleaned['Month'] = data_cleaned['InvoiceDate'].dt.month
+print(data_cleaned.head())
+
+>>>>>>> 7a81 (limpieza de datos y conversion a datos temporales)
