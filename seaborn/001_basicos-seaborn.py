db9eaa6cf1a66cddc82d7a30e56a92d6b706ded7
Gabinete <fcisneros@outlook.es>
2025-01-15 04:47:14 -0600

    graficos en seaborn

--- seaborn/001_basicos-seaborn.py
+++ seaborn/001_basicos-seaborn.py
@@ -12,18 +12,26 @@ plt.switch_backend('TkAgg')  # o plt.switch_backend('QtAgg')
 
 #  sns.set(style='dark',palette='dark',font_scale=1)
 #  sns.barplot(x='Categoria', y='Valor', data=data)
+#  ruta = os.path.expanduser("~/sheets/data_science/001_numpy_pandas_matplotlib_seaborn/graficas_seaborn")
+#  ruta_completa = os.path.join(ruta, "barplot.png")
+#  plt.savefig(ruta_completa, dpi=300, bbox_inches="tight")
 #  plt.show()
 
 tip = sns.load_dataset('tips')
 #  print(tip)
 #  print(type(tip))
 #  tip.to_csv('tip.csv', index=False, encoding='utf-8')
-#
 #  sns.displot(data=tip, x='total_bill',y='tip', hue='sex')
 #  sns.displot(data=tip, x='total_bill',hue='sex', kind='kde',legend=False,palette='dark',alpha=0.5)
 #  plt.show()
 
-#  sns.histplot(data=tip, x='tip', bins=5, cumulative=True, hue='sex', stat='frequency')
+
+sns.histplot(data=tip, x='tip', bins=5, cumulative=True, hue='sex', stat='frequency')
+plt.show()
+ruta = os.path.expanduser("~/sheets/data_science/001_numpy_pandas_matplotlib_seaborn/graficas_seaborn")
+ruta_completa = os.path.join(ruta, "004_stacked histogram.png")
+plt.savefig(ruta_completa, dpi=300, bbox_inches="tight")
+
 #  sns.histplot(data=tip, x='tip', bins=15, cumulative=False, hue='sex', stat='probability')
 #  sns.histplot(data=tip, x='tip', bins=15, cumulative=False, hue=':xsex', stat='percent')
 #  sns.histplot(data=tip, x='tip', bins=15, cumulative=False, hue='sex', stat='percent',multiple='dodge')
@@ -36,27 +44,52 @@ tip = sns.load_dataset('tips')
 #  sns.displot(data=tip, x='tip', hue='sex', kind='kde')
 #  sns.displot(data=tip, x='tip', hue='sex', kind='hist',multiple='stack')
 #  plt.show()
-
-# Variables categoricas
+#
+#  #  Variables categoricas
 #  sns.countplot(data=tip,x='day',hue='sex')
 #  sns.countplot(data=tip,y='day',hue='sex')
 #  sns.stripplot(data=tip,x='day',hue='sex')
 #  sns.stripplot(data=tip,x='day',y='total_bill',hue='sex')
 #  sns.stripplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True)
-
+#
 #  sns.swarmplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True)
 #  sns.boxplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True)
-
+#
 #  # Combinacion de ambas graficas, swarmplot y boxplot
 #  plt.figure(figsize=(6,6))
 #  sns.swarmplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True,color='0',marker='<')
 #  sns.boxplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True)
 #  plt.show()
-
+#gg
 #  plt.figure(figsize=(6,6))
 #  sns.violinplot(data=tip,x='day',y='total_bill',hue='sex',split=True,dodge=True,palette='pastel')
 #  plt.show()
-
-sns.catplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True,kind='box',col='time')
+#
+#  sns.catplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True,kind='box',col='time')
 #  sns.catplot(data=tip,x='day',y='total_bill',hue='sex',dodge=True,kind='swarm',col='time')
-plt.show()
+#  plt.show()
+
+#  plt.figure(figsize=(8,6))
+#  markers = {'Lunch':'D','Dinner':'s'}
+#  sns.scatterplot(data=tip,x='total_bill',y='tip',hue='day',style='time',size='size',markers=markers)
+#  plt.legend(loc='center',bbox_to_anchor=(1.09,0.5))
+#  plt.show()
+#
+#  sns.lineplot(data=tip,x='total_bill',y='tip')
+#  plt.show()
+#
+#  markers = {'Lunch':'D','Dinner':'s'}
+#  sns.relplot(data=tip,x='total_bill',y='tip',hue='day',style='time',size='size',markers=markers,height=6,aspect=1.3)
+#  plt.show()
+#
+#  sns.jointplot(data=tip, x='total_bill',y='tip')
+#  sns.jointplot(data=tip, x='total_bill',y='tip',hue='sex')
+#  sns.jointplot(data=tip, x='total_bill',y='tip',hue='sex',kind='hist')
+#  sns.jointplot(data=tip, x='total_bill',y='tip',hue='sex',kind='kde')
+#  sns.jointplot(data=tip, x='total_bill',y='tip',hue='sex',kind='hist',marginal_ticks=True,marginal_kws=dict(bins=25,fill=False,multiple='dodge'))
+#  plt.show()
+#
+#  #  Pairplot (relaciones de variables numericas en distintas graficas)
+#  sns.pairplot(data=tip)
+#  sns.pairplot(data=tip,hue='sex',corner=True,kind='scatter')
+#  plt.show()
